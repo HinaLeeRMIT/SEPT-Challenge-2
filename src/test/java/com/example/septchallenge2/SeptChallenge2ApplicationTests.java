01e098d09d25ff0da199ae1f88e6b8a1b27b8bf8
Yeuni <hinalee5250@gmail.com>
2022-10-10 20:37:12 +1100

    Base skeleton code

---
+++ src/test/java/com/example/septchallenge2/SeptChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallenge2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
